ddabdf7f1944cf9303dd9a5b3a742473583fd9a0
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-02-02 20:49:34 -0500

    Start creating game->celltype mapping

--- src/main/java/cellsociety/util/GameCellMapping.java
+++ src/main/java/cellsociety/util/GameCellMapping.java
@@ -1,5 +1,14 @@
 package cellsociety.util;
 
-public class GameCellMapping {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class GameCellMapping {
+	private static HashMap<Type.GAMETYPE, ArrayList<Type.CELLTYPE>> mapping;
+
+	public GameCellMapping() {
+		mapping.put(Type.GAMETYPE.GAMEOFLIFE, new ArrayList<>(Arrays.asList(Type.CELLTYPE.DEAD, Type.CELLTYPE.ALIVE)));
+		mapping.put(Type.GAMETYPE.GAMEOFLIFE, new ArrayList<>(Arrays.asList(Type.CELLTYPE.DEAD, Type.CELLTYPE.ALIVE)));
+	}
 }
