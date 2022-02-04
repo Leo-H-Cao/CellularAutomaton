1213b63821a23f41535ce476fc281de2535ad02e
Nolan Gelinas <nolan.gelinas@duke.edu>
2022-02-04 02:15:04 -0500

    Change default selectedType

--- src/main/java/cellsociety/view/GameCellMapping.java
+++ src/main/java/cellsociety/view/GameCellMapping.java
@@ -9,14 +9,15 @@ import static cellsociety.cell.CellType.*;
 import static cellsociety.game.GameType.*;
 
 public class GameCellMapping {
-	public final Map<GameType, ArrayList<CellType>> MAP;;
+	public final Map<GameType, ArrayList<CellType>> MAP;
+
 
 	public GameCellMapping() {
 		MAP = new HashMap<>();
-		MAP.put(GAMEOFLIFE, new ArrayList<>(Arrays.asList(DEAD, ALIVE)));
-		MAP.put(FIRE, new ArrayList<>(Arrays.asList(EMPTY, BURNING, TREE)));
-		MAP.put(PERCOLATION, new ArrayList<>(Arrays.asList(BLOCK, WATER)));
-		MAP.put(WATOR, new ArrayList<>(Arrays.asList(EMPTY, FISH, SHARK)));
-		MAP.put(SCHELLSEG, new ArrayList<>(Arrays.asList(EMPTY, A, B)));
+		MAP.put(GAMEOFLIFE, new ArrayList<>(Arrays.asList(ALIVE, DEAD)));
+		MAP.put(FIRE, new ArrayList<>(Arrays.asList(BURNING, TREE, EMPTY)));
+		MAP.put(PERCOLATION, new ArrayList<>(Arrays.asList(WATER, BLOCK)));
+		MAP.put(WATOR, new ArrayList<>(Arrays.asList(FISH, SHARK, EMPTY)));
+		MAP.put(SCHELLSEG, new ArrayList<>(Arrays.asList(A, B, EMPTY)));
 	}
 }
